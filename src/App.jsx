e23f3508b95005effdb48952a69691dfc51b7b88
Arthur Justino <arthur.justino.ferreira085@gmail.com>
2024-04-06 15:56:37 -0300

    Adicionado section produto e algumas mudanças

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import Header from "./components/Header";
 import Promotion from "./components/Promotion";
+import Product from "./components/Product";
 
 function App() {
   return (
     <>
       <Promotion />
       <Header />
+      <Product />
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,25 @@
+import styles from "./Product.module.css";
+import ProductImg from "../assets/product.jpg";
+
+function Product () {
+    return (
+        <section className={styles.product}>
+            <div className={styles.container}>
+                <img src={ProductImg} alt="Tênis" draggable="false" />
+                <div className={styles.info_product}>
+                    <span className={styles.hat}>NOVO MODELO</span>
+                    <div className={styles.line}></div>
+                    <h2 className={styles.product_name}>TÊNIS ESPORTIVO</h2>
+                    <p className={styles.description}>Novo tênis esportivo altamente tecnológico, com o maior conforto que se pode proporcionar. Fortemente recomendo por especialistas no desenvolvimento de tênis esportivos!</p>
+                    <div className={styles.buttons}>
+                        <a className={styles.checkout} href="#">Ver produto</a>
+                        <a className={styles.purchase} href="#">COMPRE JÁ!</a>
+                    </div>
+                    <span className={styles.detail}>Disponível em todas as lojas físicas</span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Product;
